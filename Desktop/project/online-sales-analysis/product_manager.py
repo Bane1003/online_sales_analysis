38d29228773f46533feff7984d1da9813211439d
Branko <jankovicb1003@gmail.com>
2026-03-02 00:28:27 +0100

    Strted new repository.

---
+++ Desktop/project/online-sales-analysis/product_manager.py
@@ -0,0 +1,19 @@
+from product import Product
+class ProductManager():
+    def __init__(self):
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def show_all_products(self):
+        if self.products !=[]:
+            for i in self.products:
+                print(i.name)
+
+    def total_sum(self):
+        sum = 0
+        if self.products != []:
+            for i in self.products:
+                sum += i.price * i.quantity
+        print(f"Sum of all products: {sum}")
