91075aba2f304066558f7781d5391293f4dffaa7
Branko <jankovicb1003@gmail.com>
2026-03-02 21:37:43 +0100

    Changes in master branch

--- Desktop/project/online-sales-analysis/main.py
+++ Desktop/project/online-sales-analysis/main.py
@@ -4,11 +4,11 @@ from product import Product
 
 prom = ProductManager()
 
-p1 = Product("majica", 1500, 15)
+p1 = Product("kosulja", 1500, 15)
 p2 = Product("patike", 8500, 12)
-p3 = Product("dukserica", 2500, 13)
+p3 = Product("jakna", 2500, 13)
 p4 = Product("trenerka", 3000, 16)
-p5 = Product("lopta", 1500, 11)
+p5 = Product("reket", 1500, 11)
 
 prom.add_product (p1)
 prom.add_product (p2)
@@ -16,6 +16,5 @@ prom.add_product (p3)
 prom.add_product (p4)
 prom.add_product (p5)
 
-prom.show_all_products()
-prom.total_sum()
+
 
